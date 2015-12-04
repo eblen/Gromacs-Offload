ca98c978e62a8540a0c77342dfe991d1e0d3ff9d
John Eblen <jeblen@acm.org>
2015-12-04 17:38:10 -0500

    Add PFun Library

---
+++ src/external/pfun/pfun.h
@@ -0,0 +1,60 @@
+#ifndef PFUN_H
+#define PFUN_H
+
+#include <atomic>
+#include <pthread.h>
+
+class IPFunTask
+{
+public:
+    virtual void run() const = 0;
+};
+
+template<typename T>
+class PFunTask : public IPFunTask
+{
+public:
+    PFunTask(const T &t) :task(t) {};
+    void run() const {task();}
+private:
+    const T &task;
+};
+
+class PFun {
+public:
+    PFun() :fun(nullptr), isRunning(false)
+    {
+        pthread_t tid;
+        pthread_create(&tid, NULL, mainLoop, this);
+    }
+
+    void run(const IPFunTask *f)
+    {
+        fun = f;
+        isRunning.store(true);
+    }
+
+    void wait() const
+    {
+        while (isRunning.load() == true);
+    }
+
+private:
+    static void *mainLoop(void *self)
+    {
+        PFun *pfun = (PFun *)self;
+        const IPFunTask *fun = pfun->fun;
+        std::atomic<bool> &isRunning = pfun->isRunning;
+        while(true)
+        {
+            while (isRunning.load() == false);
+            fun->run();
+            isRunning.store(false);
+        }
+    }
+
+    const IPFunTask *fun;
+    std::atomic<bool> isRunning;
+};
+
+#endif // PFUN_H
